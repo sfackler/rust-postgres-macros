3b88cc49bf47b481157402629a786b7706cb88e1
Steven Fackler <sfackler@gmail.com>
2014-11-07 22:42:17 -0800

    Fix deadlock in builder

--- build.rs
+++ build.rs
@@ -1,8 +1,14 @@
 use std::io::Command;
+use std::io::process::StdioContainer::InheritFd;
 use std::os;
 
 fn main() {
-    Command::new("make").status().unwrap();
+    Command::new("make")
+        .stdin(InheritFd(0))
+        .stdout(InheritFd(1))
+        .stderr(InheritFd(2))
+        .status()
+        .unwrap();
     let out_dir = os::getenv("OUT_DIR").unwrap();
     println!("cargo:rustc-flags=-L {} -l parser:static", out_dir);
 }
